7a6e1358c23954c306952543ad1179ca304c9e58
serkbugs <serkbugs@narod.ru>
2015-02-26 00:42:50 +0300

    added install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp ./ups-plasma/build/lib/plasma_engine_upsdata.so /usr/lib64/kde4
+cp ./ups-plasma/src/upsdataengine/plasma-engine-upsdata.desktop /usr/share/kde4/services
+
+#copy applet
+cp ./ups-plasma/build/lib/libplasma_applet_ups_monitor.so /usr/lib64/kde4
+cp ./ups-plasma/src/upsplasmoid/plasma-applet-ups-monitor.desktop /usr/share/kde4/services
